36e9feaa55743e1e6086048e133c9bf2fb893870
jason <jason.wanjohi@savannahinformatics.com>
2015-05-21 07:52:56 +0300

    Removed unnecessary filters
    These filters became unnecessary after the back-end evolved and raised page sizes
    for GIS

--- src/app/gis/country.controllers.js
+++ src/app/gis/country.controllers.js
@@ -27,27 +27,22 @@
                 icon: "fa-arrow-left"
             }
         ];
-        angular.extend($scope, {
-            defaults: {
-                scrollWheelZoom: false,
-                tileLayer: ""
-            },
-            events: {
-                map: {
-                    enable: ["moveend", "popupopen"],
-                    logic: "emit"
-                },
-                marker: {
-                    enable: [],
-                    logic: "emit"
-                }
-            },
-            markers:{},
-            layers:{}
-        });
-        $scope.filters_country = {
-            code: "KEN"
+        $scope.defaults = {
+            scrollWheelZoom: false,
+            tileLayer: ""
         };
+        $scope.events = {
+            map: {
+                enable: ["moveend", "popupopen"],
+                logic: "emit"
+            },
+            marker: {
+                enable: [],
+                logic: "emit"
+            }
+        };
+        $scope.markers = {};
+        $scope.layers = {};
         $scope.country_success = function (data){
             leafletData.getMap()
                 .then(function (map) {
@@ -61,17 +56,27 @@
                     $timeout(function() {map.spin(false);}, 1000);
                 });
         };
-        gisCountriesApi.api
-        .filter($scope.filters_country)
-        .success($scope.country_success)
-        .error(function (e) {
-            $scope.alert = e.error;
+    
+        // Handle mouseover and click events on the rendered map
+        $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, county) {
+            $scope.hoveredCounty = county;
         });
-        $scope.filters_counties = {
-            page_size: 47
-        };
+        $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, county) {
+            var boundary_ids = county.properties.constituency_boundary_ids.join(",");
+            $stateParams.const_boundaries = boundary_ids;
+            $state.go("gis_county",{county_id: county.id,
+                                    const_boundaries : boundary_ids});
+        });
+            
+        gisCountriesApi.api
+            .filter({code: "KEN"})
+            .success($scope.country_success)
+            .error(function (e) {
+                $scope.alert = e.error;
+            });
+        
         gisCountiesApi.api
-        .filter($scope.filters_counties)
+        .list()
         .success(function (data){
             var marks = data.results.features;
             var markers = _.mapObject(marks, function(mark){
@@ -91,16 +96,11 @@
             angular.extend($scope, {
                 layers:{
                     baselayers:{
-                        mapbox_light: {
-                            name: "Mapbox Light",
-                            url: "http://api.tiles.mapbox.com/"+
-                            "v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}",
-                            type: "xyz",
-                            layerOptions: {
-                                apikey: "pk.eyJ1IjoiYnVmYW"+
-                                "51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q",
-                                mapid: "jasonwanjohi.m706od7c"
-                            }
+                        counties:{
+                            name:"Counties",
+                            type:"group",
+                            visible: true,
+                            data: []
                         }
                     },
                     overlays:{
@@ -114,11 +114,6 @@
                                 blur: 1,
                                 gradient: {0.2: "lime", 0.3: "orange",0.4: "red"}
                             },
-                            visible: true
-                        },
-                        counties:{
-                            name:"Counties",
-                            type:"markercluster",
                             visible: true
                         }
                     }
@@ -142,12 +137,8 @@
         .error(function(err){
             $scope.alert = err.error;
         });
-
-        $scope.filters_fac = {
-            page_size:9000
-        };
         gisFacilitiesApi.api
-        .filter($scope.filters_fac)
+        .list()
         .success(function (data){
             var heats = data.results.features;
             var heatpoints = _.map(heats, function(heat){
@@ -160,15 +151,6 @@
         })
         .error(function(err){
             $scope.alert = err.error;
-        });
-        $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, county) {
-            $scope.hoveredCounty = county;
-        });
-        $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, county) {
-            var boundary_ids = county.properties.constituency_boundary_ids.join(",");
-            $stateParams.const_boundaries = boundary_ids;
-            $state.go("gis_county",{county_id: county.id,
-                                    const_boundaries : boundary_ids});
         });
     }]);
 })(angular);
